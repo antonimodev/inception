aa45cb486ef003a1483379e3e1287bada87999fa
antonimo <antonio98park@hotmail.com>
2025-11-14 19:26:37 +0100

    Update style in Dockerfile and add command to entrypoint to setting up 9000 port

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+# Configure PHP-FPM to LISTEN all interfaces
+sed -i 's/listen = .*/listen = 0.0.0.0:9000/' /etc/php/8.2/fpm/pool.d/www.conf
+
 # Execute with -F is like daemon off; in nginx
 exec php-fpm8.2 -F
