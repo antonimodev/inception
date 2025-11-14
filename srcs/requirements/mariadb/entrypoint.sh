bb9797c6ec4654389a901eb9b704102f440e2ecc
antonimo <antonio98park@hotmail.com>
2025-11-14 19:24:45 +0100

    Change commands from entrypoint to Dockerfile

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
-
 # If this directory doesn't exist, create Database directory (contains tables, etc.)
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql --datadir=var/lib/mysql
